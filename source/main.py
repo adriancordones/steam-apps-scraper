71653f9b843679e0ad09bffa0b17ffe34581c912
Adrián Cordones <adriancordones@gmail.com>
2026-04-07 23:46:13 +0200

    Minor fixes 2

--- source/main.py
+++ source/main.py
@@ -5,15 +5,15 @@ from scrapy.utils.reactor import install_reactor
 from scrapy.utils.project import get_project_settings
 from twisted.internet.task import react
 
-from steam_scraper.spiders.links_spider import LinksSpider
-from steam_scraper.spiders.apps_spider import AppsSpider
+from steam_apps_scraper.spiders.links_spider import LinksSpider
+from steam_apps_scraper.spiders.apps_spider import AppsSpider
 
 async def crawl(_):
     settings = get_project_settings()
     runner = AsyncCrawlerRunner(settings)
 
     crawler = runner.create_crawler(LinksSpider)
-    await runner.crawl(crawler, srt_page=1, end_page=2)
+    await runner.crawl(crawler, srt_page=1, end_page=1000)
     await runner.crawl(AppsSpider, app_ids=crawler.spider.app_ids)
 
 configure_logging({"LOG_FORMAT": "%(levelname)s: %(message)s"})
